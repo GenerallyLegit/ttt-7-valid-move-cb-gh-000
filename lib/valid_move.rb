1d341e1cd55d757f777de1ad3e399ea8d42a870b
Niall McPaul <niall.mcpaul@queens.ox.ac.uk>
2018-03-06 21:18:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, input)
-  if input.between?(0, 8) && position_taken?(board, input) == true
+  if input.between?(0, 8)
     true
   else
     false
